6d78717f35fad28b9efb5c93a96f2c0c53d6e7c0
Yota Toyama <raviqqe@gmail.com>
2016-12-08 05:11:31 +0900

    Complete prototype of my universe

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+variable "project" {
+  default = "navium-151723"
+}
+
+variable "num_instances" {
+  default = 2
+}
+
 provider "google" {
-  project = "navium-151723"
-  region = "us-central1"
-  credentials = "${file("~/.google/navium.json")}"
+  project     = "${var.project}"
+  region      = "us-central1"
+  credentials = "${file("~/.google/${var.project}.json")}"
 }
 
 resource "google_compute_instance" "navium" {
-  count = 2
+  count = "${var.num_instances}"
 
   name         = "navis-${count.index}"
   machine_type = "f1-micro"
@@ -16,7 +24,8 @@ resource "google_compute_instance" "navium" {
   }
 
   network_interface {
-    network = "default"
+    subnetwork = "${google_compute_subnetwork.default.name}"
+    address    = "10.0.0.${count.index + 2}"
     access_config {}
   }
 
@@ -29,10 +38,31 @@ resource "google_compute_instance" "navium" {
       type        = "ssh"
       user        = "root"
       private_key = "${file("~/.ssh/id_rsa")}"
+      agent       = "false"
     }
 
     inline = [
       "sudo apt -y install docker",
     ]
+  }
+}
+
+resource "google_compute_network" "default" {
+  name = "mare"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "caribbean"
+  ip_cidr_range = "10.0.0.0/8"
+  network       = "${google_compute_network.default.self_link}"
+}
+
+resource "google_compute_firewall" "default" {
+  name = "fireball"
+  network = "${google_compute_network.default.name}"
+
+  allow {
+    protocol = "tcp"
+    ports = [22, 80, 443]
   }
 }
